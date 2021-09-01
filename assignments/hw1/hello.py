bc25daed521afec810ac237aae016328d4592c56
shelbyrhodes <rhodessl@g.cofc.edu>
2021-09-01 19:40:43 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name:Shelby Rhodes
+hello.py
+
+Problem: This program prints “hello, world!” to the screen
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+
+main()
