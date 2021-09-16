a617dcbd5dac9a930905eb1a6695760d4aaf7c1f
shelbyrhodes <rhodessl@g.cofc.edu>
2021-09-16 15:33:35 -0400

    completed lab3

--- labs/lab3/lab3.py
+++ labs/lab3/lab3.py
@@ -1,4 +1,53 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Shelby Rhodes
+lab3.py
 """
+
+def average():
+    HW = eval(input("Number of grades to input:"))
+    acc = 0
+    for i in range(HW):
+        grade = eval(input("Enter your grade on HW" +str(i + 1)))
+        acc = grade + acc
+    average =  acc / HW
+    print(average)
+
+average()
+
+def tip_jar():
+    acc = 0
+    for i in range(1, 6):
+        donation = eval(input("Amount of donation:"))
+        acc = donation + acc
+    print(acc)
+
+tip_jar()
+
+def newton():
+    x = eval(input("Number:"))
+    refine = eval(input("How many times:"))
+    approx = x / 2
+    for i in range(refine):
+        approx = (approx + x/approx)/2
+    print(approx)
+
+newton()
+
+def sequence():
+    n = eval(input("What is the number of terms"))
+    for x in range(n):
+        y = 1 + ((x + 1) // 2 * 2)
+        print(y)
+
+sequence()
+
+def pi():
+    acc = 1
+    terms = eval(input("What is the number of terms"))
+    for x in range(terms):
+        num = 2 + (x // 2 * 2)
+        den = 1 + ((x + 1) // 2 * 2)
+        acc = acc * (num/den)
+    print(acc * 2)
+
+pi()
